bfb42711fac0920245cb70fe41d89f9f7c375de4
Anders Fisher <atleastimtrying@gmail.com>
2014-11-11 11:34:44 +0000

    invoice builder demo setup

---
+++ script.js
@@ -0,0 +1,54 @@
+window.builder = {
+  input_names: [
+    'invoice_id',
+    'date',
+    'client_name',
+    'client_address',
+    'work_description',
+    'excl_vat_price'
+  ]
+};
+window.builder.App = function(){
+  var by_id = function(id){
+    return document.getElementById(id);
+  };
+  var init = function(){
+    bind_ui();
+  };
+  var bind_ui = function(){
+    by_id('invoice_data').addEventListener('submit', process_form, false);
+  };
+  var get_values = function(){
+    var values = {};
+    builder.input_names.forEach(function(name){
+      values[name] = get_value(name);
+    });
+    return values;
+  };
+  var get_value = function(name){
+    return by_id( name+ '_input').value
+  };
+  var add_vat = function(values){
+    values.excl_vat_price = parseInt(values.excl_vat_price);
+    values.incl_vat_price = values.excl_vat_price * 1.2;
+    return values;
+  };
+  var render = function(values){
+    for(name in values){
+      var value = values[name];
+      by_id(name).innerHTML = value;
+    }
+  };
+  var process_form = function(event){
+    event.preventDefault();
+    var values = get_values();
+    var vat_values = add_vat(values);
+    render(vat_values);
+    window.print();
+  };
+  init();
+};
+
+window.addEventListener('load', function(){
+  window.app = new window.builder.App();
+}, false);

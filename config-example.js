f835e83fdcf475323f1062c7cd215e7561b52e27
Adam Whittingham <adam.whittingham@gmail.com>
2014-12-03 15:47:14 +0000

    Add config.js for configuring sensitive information

--- config-example.js
+++ config-example.js
@@ -1,7 +1,12 @@
-window.builder.JsonReader = {
+window.config = {
+  "bank": 'bank name',
   "bank_sort_code":'sort code',
   "bank_account_number":'account number',
+  "company_name": 'company_name',
   "company_number":'company_number',
   "comapny_vat_number":'VAT number',
-  "company_incorporation_date":'incorporation date'
+  "company_incorporation_date":'incorporation date',
+  "company_directors": 'Name One and Name Two',
+  "company_address": ["line one", "line two"],
+  "payment_days": 14
 };
--- script.js
+++ script.js
@@ -8,19 +8,44 @@ window.builder = {
     'excl_vat_price'
   ]
 };
+
 window.builder.App = function(){
   var by_id = function(id){
     return document.getElementById(id);
   };
+
   var init = function(){
     bind_ui();
   };
+
   var bind_ui = function(){
     by_id('invoice_data').addEventListener('submit', process_form, false);
+    insert_secret_details();
   };
+
+  var insert_secret_details = function(){
+    var config = window.config;
+    for(var info in config){
+      var value = config[info];
+      var elements = document.getElementsByClassName(info);
+      if(elements.length > 0){
+        for(var i=0, l=elements.length; i<l; i++){
+          var el = elements[i];
+          if(value instanceof Array){
+            value = value.join(', ');
+          }
+          el.innerHTML = value;
+        }
+      }else{
+        alert("Cannot find id \'"+ id +"\' to insert data into. Check the key name in config.js");
+      }
+    }
+  };
+
   var add_title = function(title){
     document.getElementsByTagName('title')[0].innerHTML = title;
   };
+
   var get_values = function(){
     var values = {};
     builder.input_names.forEach(function(name){
@@ -28,25 +53,29 @@ window.builder.App = function(){
     });
     return values;
   };
+
   var get_value = function(name){
-    return by_id( name+ '_input').value
+    return by_id( name+ '_input').value;
   };
+
   var add_vat = function(values){
     values.excl_vat_price = parseInt(values.excl_vat_price);
     values.incl_vat_price = values.excl_vat_price * 1.2;
     values.vat_charged = values.excl_vat_price * 0.2;
     return values;
   };
+
   var render = function(values){
-    for(name in values){
+    for(var name in values){
       var value = values[name];
       by_id(name).innerHTML = value;
     }
   };
+
   var process_form = function(event){
     event.preventDefault();
     var values = get_values();
-    add_title(values['invoice_id']);
+    add_title(values.invoice_id);
     var vat_values = add_vat(values);
     render(vat_values);
     window.print();

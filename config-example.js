75519e826d5c78c58c714cc431471c2596767940
Adam Whittingham <adam.whittingham@gmail.com>
2014-12-03 14:28:51 +0000

    Add config.js loading

---
+++ config-example.js
@@ -0,0 +1,7 @@
+window.builder.JsonReader = {
+  "bank_sort_code":'sort code',
+  "bank_account_number":'account number',
+  "company_number":'company_number',
+  "comapny_vat_number":'VAT number',
+  "company_incorporation_date":'incorporation date'
+};
